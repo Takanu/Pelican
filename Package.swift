1a8197dddd527c563a33f7c12b81007124f9e21f
Takanu Kyriako <ido@Takanus-iMac.home>
2017-05-21 23:31:01 +0100

    Vapor 2 Changes
    Trying to get SPM to work…

--- Package.swift
+++ Package.swift
@@ -2,6 +2,7 @@ import PackageDescription
 
 let package = Package(
     name: "Pelican",
+    targets: [],
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
         .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1)
