d64070a8ba13947b2b00755c4335485f2766b69d
Takanu Kyriako <9108903+Takanu@users.noreply.github.com>
2017-10-22 20:04:23 +0100

    (Forgot to include the new CasedEnum utility file)

---
+++ Sources/Pelican/Utilities/CasedEnum.swift
@@ -0,0 +1,31 @@
+//
+//  CasedEnum.swift
+//  Pelican
+//
+//  Created by Ido Constantine on 20/10/2017.
+//
+
+import Foundation
+
+/**
+Defines convenience iteration and string extraction methods for various enumeration types, such as UpdateType.
+*/
+public protocol CasedEnum : Hashable {
+	
+	func string() -> String
+}
+
+extension CasedEnum {
+	static func cases() -> AnySequence<Self> {
+		typealias S = Self
+		return AnySequence { () -> AnyIterator<S> in
+			var raw = 0
+			return AnyIterator {
+				let current : Self = withUnsafePointer(to: &raw) { $0.withMemoryRebound(to: S.self, capacity: 1) { $0.pointee } }
+				guard current.hashValue == raw else { return nil }
+				raw += 1
+				return current
+			}
+		}
+	}
+}
