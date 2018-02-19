132c910b2849402d3dbcc0957459dd35a6268cc2
Takanu Kyriako <takanu@users.noreply.github.com>
2018-02-19 12:33:10 +0000

    - Moved to the latest Version 2 release of Vapor.

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
 			.library(name: "Pelican", targets: ["Pelican"]),
     ],
     dependencies: [
-			.package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
+			.package(url: "https://github.com/vapor/vapor.git", from: "2.0.0")
 			.package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
     ],
     targets: [
