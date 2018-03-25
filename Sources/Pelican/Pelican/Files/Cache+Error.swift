6d41c6b6968d1c4b1c295470dd8a07c4552f54a4
Takanu Kyriako <Takanu@users.noreply.github.com>
2018-03-25 23:47:34 +0100

    Capitilzed URL because I'm pedantic (:

--- Sources/Pelican/Pelican/Files/Cache+Error.swift
+++ Sources/Pelican/Pelican/Files/Cache+Error.swift
@@ -9,7 +9,7 @@ import Foundation
 
 // Errors related to update processing.  Might merge the two?
 enum CacheError: String, Error {
-	case BadPath = "Can't build url from path that was provided."
+	case BadPath = "Can't build URL from path that was provided."
 	case BadBundle = "The cache path for Telegram could not be found.  Please ensure Public/ is a folder in your project directory."
 	case WrongType = "The file could not be added because it has the wrong type."
 	case LocalNotFound = "The local resource you attempted to upload could not be found."
